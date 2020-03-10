6a4f96c0543f5614f1a5582276c0486b7e3c8c95
drospond <david.rospond@gmail.com>
2020-03-10 17:47:19 -0400

    Added layout for navbar and Your score page

---
+++ script.js
@@ -0,0 +1,12 @@
+var startButton = document.querySelector("#start-button");
+
+var score;
+
+startButton.addEventListener("click", testFunciton);
+
+function testFunciton(event){
+}
+
+//store high scores in local storage as JSON
+// localStorage.setItem("user", JSON.stringify(user));
+// var lastUser = JSON.parse(localStorage.getItem("user"));
