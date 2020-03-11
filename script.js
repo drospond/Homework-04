ba86998a7be73f66189d95ca6d9c913eb1eb5c9e
drospond <david.rospond@gmail.com>
2020-03-10 21:46:50 -0400

    View highscore and go back button funcitonality added

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
+//button elements
 var startButton = document.querySelector("#start-button");
+var highscoreButton = document.getElementById("highscore-button");
+var backButton = document.getElementById("back-button");
+
+//page elements
+var homePage = document.getElementById("home-page");
+var questionPage = document.getElementById("question-page");
+var scorePage = document.getElementById("score-page");
+var highscorePage = document.getElementById("highscore-page");
 
 var score;
 
-startButton.addEventListener("click", testFunciton);
+//button functions
+function viewHighscores(event){
+    homePage.classList.add("d-none");
+    questionPage.classList.add("d-none");
+    highscorePage.classList.remove("d-none");
+}
+
+function back(event){
+    highscorePage.classList.add("d-none");
+    homePage.classList.remove("d-none");
+}
 
 function testFunciton(event){
+    console.log("test");
 }
+
+highscoreButton.addEventListener("click", viewHighscores);
+startButton.addEventListener("click", testFunciton);
+backButton.addEventListener("click", back);
+
 
 //store high scores in local storage as JSON
 // localStorage.setItem("user", JSON.stringify(user));
 // var lastUser = JSON.parse(localStorage.getItem("user"));
+
